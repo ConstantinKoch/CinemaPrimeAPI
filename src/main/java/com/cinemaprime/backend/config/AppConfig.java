fe09b5f3a03f51227b2f0f600611338ac49db61f
Constantin Koch <constantin.koch@curemannheim.de>
2021-02-08 12:28:00 +0100

    Using hosted db now

--- src/main/java/com/cinemaprime/backend/config/AppConfig.java
+++ src/main/java/com/cinemaprime/backend/config/AppConfig.java
@@ -2,6 +2,7 @@ package com.cinemaprime.backend.config;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -9,7 +10,7 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 @Configuration
 public class AppConfig {
     public @Bean MongoClient mongoClient() {
-        return MongoClients.create("mongodb://root:password@localhost:27020");
+        return MongoClients.create("mongodb+srv://cinemaprime:MeinKinoIstSicher@cluster0.uris2.mongodb.net/cinemaprime?retryWrites=true&w=majority");
     }
 
     public @Bean MongoTemplate mongoTemplate() {
